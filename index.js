e475d9ee1a0c1aac9bd86ba732b2597c918a9f9d
Simen Bekkhus <sbekkhus91@gmail.com>
2019-01-22 22:05:35 +0100

    Add support for running inside of worker threads

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ var onExit = require('signal-exit')
 var path = require('path')
 var activeFiles = {}
 
+// if we run inside of a worker_thread, `process.pid` is not unique
+/* istanbul ignore next */
+var threadId = (function getId () {
+  try {
+    var workerThreads = require('worker_threads')
+
+    /// if we are in main thread, this is set to `0`
+    return workerThreads.threadId
+  } catch (e) {
+    // worker_threads are not available, fallback to 0
+    return 0
+  }
+})()
+
 var invocations = 0
 function getTmpname (filename) {
   return filename + '.' +
     MurmurHash3(__filename)
       .hash(String(process.pid))
+      .hash(String(threadId))
       .hash(String(++invocations))
       .result()
 }
