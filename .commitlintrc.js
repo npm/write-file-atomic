a49e8155f6bd44855589f8645777185d8f442633
Gar <gar+gh@danger.computer>
2022-02-09 11:56:02 -0800

    chore: @npmcli/template-oss@2.7.1

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -6,6 +6,6 @@ module.exports = {
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'chore', 'deps']],
     'header-max-length': [2, 'always', 80],
-    'subject-case': [2, 'always', ['lower-case', 'sentence-case', 'start-case']],
+    'subject-case': [0, 'always', ['lower-case', 'sentence-case', 'start-case']],
   },
 }
